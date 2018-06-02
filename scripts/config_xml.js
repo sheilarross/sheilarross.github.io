1d7b0b21d89810204855834f19d395c8a34e0cf6
sheilarross <39847027+sheilarross@users.noreply.github.com>
2018-06-02 11:38:34 -0500

    Now with bookmarks

--- scripts/config_xml.js
+++ scripts/config_xml.js
@@ -2,7 +2,7 @@ var TSC = TSC || {};
 
 TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
-      <rdf:Description dc:date="2018-06-01 11:46:41 PM" dc:source="Camtasia,9.1.2,enu" dc:title="screencastspecial" tscDM:firstFrame="screencastspecial_First_Frame.png" tscDM:originId="14F760C8-5713-4E79-9B70-B1E1AECB3D2C" tscDM:project="giantproject">\
+      <rdf:Description dc:date="2018-06-02 11:29:48 AM" dc:source="Camtasia,9.1.2,enu" dc:title="giantproject" tscDM:firstFrame="giantproject_First_Frame.png" tscDM:originId="14F760C8-5713-4E79-9B70-B1E1AECB3D2C" tscDM:project="giantproject">\
          <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="2562500"/>\
          <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="720" stDim:w="1080"/>\
          <tsc:langName>\
@@ -12,11 +12,19 @@ TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta
          <xmpDM:Tracks>\
             <rdf:Bag>\
                <rdf:li>\
-                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="082C704C-1C55-487F-A607-6D009B5E352A" tscIQ:authoredEmail="SXAY4e40EG1OdXEP+CA2HS8iZybIkESmYiwRCeKG4e+4zad30OF9pOBt6LdgIUXgEUYT2IH0/M19\
-io0Mkok6w8zlasT/mbrI4Rlg1Htc4G+QOo5rT8fjUQtJlQbkcqYiOT6BBaEQ+Fk/4+0GEDJUUo61\
-hZhbwJL+i/cxCTotoxtSe37Xj5ZzOig2SVIwo8MorO/q2Ka1MTZXjj+DxQUaB+TW0vrDBI/u9qjw\
-GdCJwcF4/gHLaYRTK0d9tqN/7hnUd1FwGTagaNg2DZy/MQethY0poWO8u4A/2hd0QMcgz80rUBRU\
-mOFQSSGpyXy8gtXQVK0bXFGp5iBL6F4iYMoPrQ==" tscIQ:requireUserId="0" tscIQ:locale="en-US" tscIQ:reportMethod="NONE" tscIQ:allowSkipQuiz="0" tscIQ:clientId="1CDF22D874AC443FBBB68075B4AE31CD" tscIQ:hideReplay="0" tscIQ:quizHash="9e903f1c3b0f2635facf573e6842d149">\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Introduction" xmpDM:startTime="0"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Recording" xmpDM:startTime="153400"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Editing" xmpDM:startTime="553467"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Producing" xmpDM:startTime="1215700"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Features to Support EF" xmpDM:startTime="1476500"/></rdf:li><rdf:li><rdf:Description xmpDM:name="EF Activators" xmpDM:startTime="1577167"/></rdf:li><rdf:li><rdf:Description xmpDM:name="EF Connectors" xmpDM:startTime="1614400"/></rdf:li><rdf:li><rdf:Description xmpDM:name="EF Self-Assessment" xmpDM:startTime="1819967"/></rdf:li><rdf:li><rdf:Description xmpDM:name="EF Multisensory Demo" xmpDM:startTime="2238000"/></rdf:li><rdf:li><rdf:Description xmpDM:name="EF Supportive Environment" xmpDM:startTime="2342267"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Pitfalls and Conclusion" xmpDM:startTime="2423367"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="28FF21E5-C6E6-45C2-8818-8C213F04153B" tscIQ:authoredEmail="We7tbJ+TT71vVa7uWXQeKEfHyIHWBD4bOREhqXlY8n9784foWfOkV3eCoK3rNn/V4BeJUFvR6EXm\
+rZkFoVaZTgEi6flP49rVPyNFOmkMdFdlcq9BMdLoshETsllKs9cXWouasS62qePbxY2DSv+4yc3i\
+Avg8JLSWD16dvW1HZ+fT+iRH6z1pFNEm8s18fpyt8vVmSm+L3MkWNSxt0Uw3UVcPxnlWss7JlTTY\
+4yM0crUsApPHfSYZFWjsuPqW0+Lywi7hTYRaLCBO8XMH3qQ/aZuLqah7blz4MVt+IYU7U9MyBobd\
+pDrDpp37a11DSf+MvAeF3KXfC4RGD4tfZFCOag==" tscIQ:requireUserId="0" tscIQ:locale="en-US" tscIQ:reportMethod="NONE" tscIQ:allowSkipQuiz="0" tscIQ:clientId="1CDF22D874AC443FBBB68075B4AE31CD" tscIQ:hideReplay="0" tscIQ:quizHash="6bbf0e1224b20f552e4f658c602de485">\
                      <xmpDM:markers>\
                         <rdf:Seq>\
                            <rdf:li><rdf:Description xmpDM:startTime="1822900" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 6"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="0"><tscIQ:question>What was shown in the activator video for this screencast?</tscIQ:question><tscIQ:correctAnswer>2</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>Race Cars</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>Robots</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="2"><tscIQ:answer>Rabbits</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li>Great job!</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect"><xmpDM:name><rdf:Alt><rdf:li>Replay the first 20 seconds of this screencast to see the answer.</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
@@ -41,7 +49,7 @@ mOFQSSGpyXy8gtXQVK0bXFGp5iBL6F4iYMoPrQ==" tscIQ:requireUserId="0" tscIQ:locale="
             <rdf:Description>\
                <tscDM:files>\
                   <rdf:Seq>\
-                     <rdf:li xmpDM:name="0" xmpDM:value="screencastspecial.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="screencastspecial_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="screencastspecial_Thumbnails.png"/></rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="giantproject.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="giantproject_First_Frame.png"/></rdf:Seq>\
                </tscDM:files>\
             </rdf:Description>\
          </tscDM:contentList>\
